0a9bf59eb8ba1e4ac345e09b328d2729cd90cad2
Александр Ткачук <aleksandr.tkachuk2@yandex.ru>
2021-05-15 11:30:03 +0300

    Add static and media settings

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,5 +83,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
