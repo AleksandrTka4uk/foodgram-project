1f8c733e72dd81a39a44ffcf69fac2849f262b36
Александр Ткачук <aleksandr.tkachuk2@yandex.ru>
2021-06-30 20:54:04 +0300

    Add recipes_format_text tag for myFollow.html template.

--- apps/recipes/templatetags/recipes_filters.py
+++ apps/recipes/templatetags/recipes_filters.py
@@ -62,3 +62,14 @@ def tags_off_params(request):
 @register.simple_tag
 def tag_in_recipe(recipe, tag_title):
     return recipe.tag.filter(title=tag_title).exists()
+
+
+@register.simple_tag
+def recipes_format_text(recipes_count):
+    remainder = recipes_count % 10
+    if remainder == 0 or remainder >= 5 or (10 <= recipes_count <= 19):
+        return f'{recipes_count} рецептов'
+    elif remainder == 1:
+        return f'{recipes_count} рецепт'
+    else:
+        return f'{recipes_count} рецепта'
