53e2913c68c4cfd4eac4d69cba4ce4b1038e71a2
Александр Ткачук <aleksandr.tkachuk2@yandex.ru>
2021-07-29 18:50:49 +0300

    Fix ALLOWED_HOSTS

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -11,9 +11,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = [
     '127.0.0.1',
