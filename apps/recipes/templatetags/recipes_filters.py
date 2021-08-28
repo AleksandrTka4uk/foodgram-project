ebb419be3ee65d7c070dc0da85d1180461cbf596
Александр Ткачук <aleksandr.tkachuk2@yandex.ru>
2021-08-28 11:25:53 +0300

    Fix recipes_format_text

--- apps/recipes/templatetags/recipes_filters.py
+++ apps/recipes/templatetags/recipes_filters.py
@@ -15,10 +15,9 @@ def is_signed_to_author(author, user):
 
 @register.simple_tag
 def recipes_format_text(recipes_count):
-    remainder = recipes_count % 10
+    remainder = recipes_count % 100
     if remainder == 0 or remainder >= 5 or (10 <= recipes_count <= 19):
         return f'{recipes_count} рецептов'
-    elif remainder == 1:
+    if remainder == 1:
         return f'{recipes_count} рецепт'
-    else:
-        return f'{recipes_count} рецепта'
+    return f'{recipes_count} рецепта'
